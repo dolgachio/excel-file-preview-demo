ccc95c9b6efcb79c1883fa5752cb60413a8bc30c
Stanislav_Dolgachov <Stanislav_Dolgachov@EPUAKYIW3579>
2020-09-18 15:34:30 +0300

    Updates

---
+++ precache-manifest.84d8b6cb2fc95c01af19aa6702de72eb.js
@@ -0,0 +1,53 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12578787eb3aee3c64c83ec7daf9feba",
+    "url": "/excel-file-preview-demo/index.html"
+  },
+  {
+    "revision": "0f8a09618402f3bde90f",
+    "url": "/excel-file-preview-demo/static/css/2.5f52ec34.chunk.css"
+  },
+  {
+    "revision": "307e01a013b0879a8393",
+    "url": "/excel-file-preview-demo/static/css/3.7248e251.chunk.css"
+  },
+  {
+    "revision": "6a5c89a223cf53d5c5b6",
+    "url": "/excel-file-preview-demo/static/css/main.3becec29.chunk.css"
+  },
+  {
+    "revision": "0f8a09618402f3bde90f",
+    "url": "/excel-file-preview-demo/static/js/2.d3fe5f86.chunk.js"
+  },
+  {
+    "revision": "567550e9e68dbf251c3c4ae0f215d37d",
+    "url": "/excel-file-preview-demo/static/js/2.d3fe5f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307e01a013b0879a8393",
+    "url": "/excel-file-preview-demo/static/js/3.c4dafeae.chunk.js"
+  },
+  {
+    "revision": "8ea379ff00c4789c453c",
+    "url": "/excel-file-preview-demo/static/js/4.fce1d0d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/excel-file-preview-demo/static/js/4.fce1d0d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5c89a223cf53d5c5b6",
+    "url": "/excel-file-preview-demo/static/js/main.1d57351e.chunk.js"
+  },
+  {
+    "revision": "c9b36f3f78202d6d1387",
+    "url": "/excel-file-preview-demo/static/js/runtime-main.81c05986.js"
+  },
+  {
+    "url": "/excel-file-preview-demo/static/media/58eaeb4e52248a5c75936c6f4c33a370.58eaeb4e.svg"
+  },
+  {
+    "revision": "7570d0c22c9ef278213cea43a9a1d12c",
+    "url": "/excel-file-preview-demo/static/media/index.7570d0c2.less"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/excel-file-preview-demo/precache-manifest.82828826a6656437f48a814ded3cdaff.js"
+  "/excel-file-preview-demo/precache-manifest.84d8b6cb2fc95c01af19aa6702de72eb.js"
 );
 
 self.addEventListener('message', (event) => {
